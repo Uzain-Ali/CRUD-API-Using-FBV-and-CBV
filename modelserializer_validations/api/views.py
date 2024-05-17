4ddd282ab60fcee1228e78dc7e33533db8521a3e
Uzain95 <uzainali95@gmail.com>
2024-05-16 21:59:40 -0700

    Built-in Validations

---
+++ modelserializer_validations/api/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render
+import io
+from rest_framework.parsers import JSONParser
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+# Fetching data from database
+@csrf_exempt
+def  student_api(request):
+    # Read
+    if request.method == 'GET':
+        json_data = request.body #JSON data come from request
+        stream = io.BytesIO(json_data) #Binary form of data in memory
+        python_data = JSONParser().parse(stream) #stream got converted into python_native_data_type
+        id = python_data.get('id', None) #fetch id from python data
+        if id is not None: #check id != none, means id exists
+            stu = Student.objects.get(id=id) #matching the id with the objects in the model
+            serializer = StudentSerializer(stu) #Python data convert into complex data
+            json_data = JSONRenderer().render(serializer.data)   #rendering data from complex data    
+            return HttpResponse(json_data, content_type = 'application/json') #Sending Response to Request
+        stu = Student.objects.all() #if id not exist it'll send complete data
+        serializer = StudentSerializer(stu, many=True) #converting complete data into complex form
+        json_data = JSONRenderer().render(serializer.data)   #rendering data from complex data     
+        return HttpResponse(json_data, content_type = 'application/json') #Sending Response to Request
+
+    # Create
+    if request.method == 'POST':
+        json_data = request.body
+        stream =io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        serializer = StudentSerializer(data = python_data)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data Created'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type = 'application/json')
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type = 'application/json') 
+    
+    # Update
+    if request.method == "PUT":
+        json_data = request.body
+        stream =io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id')
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu, data = python_data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg':'Data Updated!!'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type='application/json')
+        json_data = JSONRenderer().render(res)
+        return HttpResponse(json_data, content_type='application/json')
+    
+    # Delete
+    if request.method == 'DELETE':
+        json_data = request.body
+        stream =io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id')
+        stu = Student.objects.get(id=id)
+        stu.delete()
+        res = {'msg':'Data Deleted!!'}
+        # json_data = JSONRenderer().render(res)
+        # return HttpResponse(json_data, content_type = 'application/json')
+        return JsonResponse(res, safe=False)
---
+++ modelserializer_validations/myapp.py
@@ -0,0 +1,72 @@
+import requests
+import json
+
+URL = "http://127.0.0.1:8000/curdapi/"
+
+# Read
+# Function to get data by hitting the api and print it
+def get_data(id = None):
+    # initialize and empty dictionary
+    data = {}
+    # Check if the id is not none
+    if id is not None:
+        # storing the id into data variavble
+        data = {'id': id}
+    # After getting the id we have to convert it into json format
+    json_data = json.dumps(data)
+    # hitting the url for calling api to fetch data from database
+    r=requests.get(url = URL,data = json_data)
+    # Converting the data into json format
+    data = r.json()
+
+    print(data)
+    
+# get_data()
+
+# CREATE
+def post_data():
+    data = {
+        'name':'noor',
+        'roll':'110',
+        'city':'karachi',
+    }
+
+    json_data = json.dumps(data)
+    r=requests.post(url = URL,data = json_data)
+    data = r.json()
+    print(data)
+
+# post_data()
+
+
+# Update 
+def update_data():
+    data = {
+        'id':9,
+        'name':'noorulain',
+        'roll':'101',
+        'city':'Islamabad',
+    }
+
+    json_data = json.dumps(data)
+    r=requests.put(url = URL,data = json_data)
+    data = r.json()
+    print(data)
+
+update_data()
+
+
+
+# Delete
+def delete_data():
+    data = {
+        'id':3
+    }
+
+    json_data = json.dumps(data)
+    r=requests.delete(url = URL,data = json_data)
+    data = r.json()
+    print(data)
+
+
+# delete_data()
