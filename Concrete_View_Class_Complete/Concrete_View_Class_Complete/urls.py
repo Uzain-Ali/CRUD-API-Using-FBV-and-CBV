52cba9674c8245f18c5f89d5d92a612f9b7c8900
Uzain95 <uzainali95@gmail.com>
2024-05-19 23:35:55 -0700

    Concrete View Class Complete

---
+++ Concrete_View_Class_Complete/Concrete_View_Class_Complete/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from api import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('curdapi/', views.StudentListCreate.as_view()),
+    path('curdapi/<int:pk>', views.StudentRetrieveUpdateDestroy.as_view())
+]
