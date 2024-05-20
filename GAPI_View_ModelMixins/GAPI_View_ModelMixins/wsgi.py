fbb68becbe5ebcab918e3dfb0efe4aae21b44622
Uzain95 <uzainali95@gmail.com>
2024-05-19 22:37:18 -0700

    Generic API View and Mixin Separate APIs

---
+++ GAPI_View_ModelMixins/GAPI_View_ModelMixins/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for GAPI_View_ModelMixins project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GAPI_View_ModelMixins.settings')
+
+application = get_wsgi_application()
