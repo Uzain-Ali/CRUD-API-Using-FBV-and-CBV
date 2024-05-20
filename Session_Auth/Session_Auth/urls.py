fda0880bfb5e07ae52ea5b747171ecbf2441dccf
Uzain95 <uzainali95@gmail.com>
2024-05-20 06:00:55 -0700

    Session Authentication

--- Session_Auth/Session_Auth/urls.py
+++ Session_Auth/Session_Auth/urls.py
@@ -13,4 +13,6 @@ router.register('studentapi', views.StudentModelViewSet,basename = 'student')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls',
+    namespace='rest_framework'))
 ]
