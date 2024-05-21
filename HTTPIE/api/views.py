e064553d2e7f8c34880d268d87a022375cd5b57b
Uzain95 <uzainali95@gmail.com>
2024-05-21 03:38:36 -0700

    HTTPIE Methods to apply auth to apis

--- HTTPIE/api/views.py
+++ HTTPIE/api/views.py
@@ -7,8 +7,8 @@ from rest_framework.permissions import IsAuthenticated
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     # permission_classes = [AllowAny]
     # permission_classes = [IsAdminUser]
     # permission_classes = [IsAuthenticatedOrReadOnly]
