08a5eb62d8f72a3edd0e8ee375174258ae24e48b
Uzain95 <uzainali95@gmail.com>
2024-05-21 03:14:55 -0700

    Token Auth Using Signals

--- Token_AUTH_USING_SIGNALS/Token_AUTH_USING_SIGNALS/settings.py
+++ Token_AUTH_USING_SIGNALS/Token_AUTH_USING_SIGNALS/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,15 @@ WSGI_APPLICATION = 'Token_AUTH_USING_SIGNALS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_crud_apis',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS':{
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
