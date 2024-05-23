f58cf7b190f1c45c2347235d2b83e6968c1bb0b6
Uzain95 <uzainali95@gmail.com>
2024-05-23 05:10:21 -0700

    Hyperlinked Model Serializers

--- Hyperlinked_SR/manage.py
+++ Hyperlinked_SR/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Serializers_Relations.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hyperlinked_SR.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
